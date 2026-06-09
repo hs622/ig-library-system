c0fbeb8c79fdcd50c302c615054214ebcc474638
hs622 <hussainalee@icloud.com>
2026-06-09 18:26:24 +0500

    feat: add useIsMobile hook, update package.json and pnpm-lock.yaml with new dependencies, and create zod schemas for category and book validation

---
+++ types/zod.ts
@@ -0,0 +1,42 @@
+import z from "zod";
+
+export const categorySchema = z.object({
+  category: z
+    .string()
+    .min(1, { message: "category is required." })
+    .max(150, { message: "Max 150 characters long" }),
+});
+
+export const BookSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: "Please enter book title." })
+    .max(150, { message: "Max 150 character long." }),
+  authorName: z
+    .string()
+    .min(1, { message: "Please enter author name." })
+    .max(100, { message: "Max 100 character long." }),
+  shortDescription: z.string().max(300, { message: "Max 300 character long." }),
+  isbn13: z.preprocess((value) => {
+    return Number.isNaN(value) ? null : Number(value);
+  }, z.number().nullable()),
+  isbn10: z.preprocess((value) => {
+    return Number.isNaN(value) ? null : Number(value);
+  }, z.number().nullable()),
+  publisherName: z.string().optional(),
+  publicationYear: z
+    .string()
+    .regex(/^\d{4}$/, "Year must be exactly 4 digits.")
+    .transform((val) => parseInt(val, 10))
+    .pipe(
+      z
+        .number()
+        .min(1900, "Year must be 1900 or later")
+        .max(new Date().getFullYear(), "Year cannot be in future."),
+    ),
+  category: z.string(),
+});
+
+export type IcategorySchema = z.infer<typeof categorySchema>;
+export type IBookSchema = z.infer<typeof BookSchema>;
+
