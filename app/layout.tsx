82829ea358948b96d89b1cbeb85add2be86d5c3f
hs622 <hussainalee@icloud.com>
2026-06-11 20:08:02 +0500

    feat: add Toaster component for notifications in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import TooltipWrapper from "./providers/tooltip";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -33,6 +34,7 @@ export default function RootLayout({
     >
       <TooltipWrapper>
         <body className="min-h-full flex flex-col">{children}</body>
+        <Toaster />
       </TooltipWrapper>
     </html>
   );
