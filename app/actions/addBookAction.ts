a76022c358aaaadb11e8844d5f60037d0c4ab27b
hs622 <hussainalee@icloud.com>
2026-06-14 01:30:19 +0500

    feat: refactor AddBookAction to return structured response and handle errors

--- app/actions/addBookAction.ts
+++ app/actions/addBookAction.ts
@@ -2,9 +2,9 @@
 
 import clientPromise from "@/lib/mongodb";
 import { IBookSchema } from "@/types/zod";
+import { MongoServerError } from "mongodb";
 
-export const AddBook_Action = async (data: IBookSchema) => {
-  console.log(data);
+export const AddBookAction = async (data: IBookSchema): Promise<{ status: boolean, message: string } | undefined> => {
 
   const client = await clientPromise;
   const database = client.db(process.env.DATABASE_NAME);
@@ -12,17 +12,34 @@ export const AddBook_Action = async (data: IBookSchema) => {
   const collection = database.collection("books");
   collection.createIndex({ title: 1 }, { unique: true }) // unquie field
 
-  const document = {
-    ...data,
-    "createdAt": new Date(),
-    "updatedAt": new Date(),
-    "deletedAt": null
-  }
+  try {
+    const document = {
+      ...data,
+      "createdAt": new Date(),
+      "updatedAt": new Date(),
+      "deletedAt": null
+    }
+  
+    const result = await collection.insertOne(document)
+    if(result.acknowledged) {
+      return {
+        status: true,
+        message: "book ingested successfully."
+      };
+    }
+  } catch(error) {
+    if (error instanceof MongoServerError) {
+      if (error.code === 11000) {
+        return {
+          status: false,
+          message: "Book already exist!"
+        }
+      }
+    }
 
-  const result = await collection.insertOne(document)
-  if(result.acknowledged) {
-    return true;
-  }
-
-  return false;
+    else return {
+      status: false,
+      message: "something went wrong."
+    }
+  } 
 }
