9c1cd0ef132747f571e49d9d9c6eb2e7675ae62e
hs622 <hussainalee@icloud.com>
2026-07-04 03:47:26 +0500

    feat: add initial Page component for book search and statistics display

---
+++ app/(administrator)/d/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <React.Fragment>
+        Book Search engine
+        Statictis (Books, Categories, members, e-content)
+    </React.Fragment>
+  )
+}
