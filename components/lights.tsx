296e5665f6d6e8f814d2e8811e353db144818a02
hs622 <hussainalee@icloud.com>
2026-07-06 20:00:05 +0500

    feat: add GreenLamp, RedLamp, and YellowLamp components

---
+++ components/lights.tsx
@@ -0,0 +1,5 @@
+
+
+export const GreenLamp = () => (<div className="rounded-full w-4 h-4 dark:bg-green-300 bg-green-600 text-xs border" ></div>)
+export const RedLamp = () => (<div className="rounded-full w-4 h-4 dark:bg-red-300 bg-red-600"></div>)
+export const YellowLamp = () => (<div className="rounded-full w-4 h-4 dark:bg-yellow-300 bg-yellow-600"></div>)
