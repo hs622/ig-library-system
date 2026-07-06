b223b7b3dcae2cd1d478cab3b7ee2e9d128d73cd
hs622 <hussainalee@icloud.com>
2026-07-06 19:44:36 +0500

    feat: update CategorySchema and add BookEditSchema for enhanced category and book management

--- types/zod.ts
+++ types/zod.ts
@@ -10,18 +10,29 @@ export const CategorySchema = z.object({
     .string()
     .min(1, { message: "category is required." })
     .max(150, { message: "Max 150 characters long" }),
-  parentId: z.string(),
+  parentId: z.string().optional(),
   isAccosicated: z.boolean(),
   isParent: z.boolean(),
-  visiable: z.boolean()
+  visiable: z.boolean(),
 });
 
-export const SubCategory = z.string().min(3, "Must be a valid sub category.").max(150, "Must be less than 150 characters.")
+export const SubCategory = z
+  .string()
+  .min(3, "Must be a valid sub category.")
+  .max(150, "Must be less than 150 characters.");
 
 export const AddCategorySchema = z.object({
-  category: z.string().min(3, "category is required!").max(150, "Must be less than 150 characters."),
-  sub_category: z.array(SubCategory).optional()
-})
+  category: z
+    .string()
+    .min(3, "category is required!")
+    .max(150, "Must be less than 150 characters."),
+  sub_category: z.array(SubCategory).optional(),
+});
+
+export const CategoryForm = z.object({
+  bookId: z.string(),
+  categoryId: z.string(),
+});
 
 // ----- book ----- //
 
@@ -52,20 +63,54 @@ export const BookSchema = z.object({
         .min(1900, "Year must be 1900 or later")
         .max(new Date().getFullYear(), "Year cannot be in future."),
     ),
-  category: z.string(),
+  category: z.object({ _id: z.string(), createdAt: z.string() }),
+});
+
+export const BookEditSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: "Please enter book title." })
+    .max(150, { message: "Max 150 character long." }),
+  authorName: z
+    .string()
+    .min(1, { message: "Please enter author name." })
+    .max(100, { message: "Max 100 character long." }),
+  shortDescription: z.string().max(300, { message: "Max 300 character long." }),
+  isbn13: z.preprocess((value) => {
+    return Number.isNaN(value) ? null : Number(value);
+  }, z.number().nullable()),
+  isbn10: z.preprocess((value) => {
+    return Number.isNaN(value) ? null : Number(value);
+  }, z.number().nullable()),
+  publisherName: z.string().optional(),
+  publicationYear: z.preprocess(
+    (value) => (value === null || value === undefined ? value : String(value)),
+    z
+      .string()
+      .regex(/^\d{4}$/, "Year must be exactly 4 digits.")
+      .transform((val) => parseInt(val, 10))
+      .pipe(
+        z
+          .number()
+          .min(1900, "Year must be 1900 or later")
+          .max(new Date().getFullYear(), "Year cannot be in future."),
+      ),
+  ),
+  categoryId: z.string().min(1, "Please select the category."),
 });
 
 export const LoginSchema = z.object({
   username: z.string().min(1, "please enter the username"),
-  password: z.string().min(1, "please enter the password")
-})
-
+  password: z.string().min(1, "please enter the password"),
+});
 
 export type ISubCategory = z.infer<typeof SubCategory>;
 export type IAddCategorySchema = z.infer<typeof AddCategorySchema>;
+export type ICategoryForm = z.infer<typeof CategoryForm>;
 
 export type TRole = z.infer<typeof Role>;
 export type ICategorySchema = z.infer<typeof CategorySchema>;
 export type IBookSchema = z.infer<typeof BookSchema>;
+export type IBookEditSchema = z.infer<typeof BookEditSchema>;
 export type ILoginSchema = z.infer<typeof LoginSchema>;
 
