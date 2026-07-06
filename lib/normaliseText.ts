9652010c1c76599b82156505282c36bf996a3eab
hs622 <hussainalee@icloud.com>
2026-07-06 19:52:21 +0500

    feat: add normaliseText function for string normalization

---
+++ lib/normaliseText.ts
@@ -0,0 +1,9 @@
+export function normaliseText(str: string): string {
+  return str
+    .toLowerCase()
+    .replace(/[\u2018\u2019]/g, "'")
+    .replace(/[\u201C\u201D]/g, '"')
+    .replace(/\s+/g, " ")
+    .trim();
+}
+
