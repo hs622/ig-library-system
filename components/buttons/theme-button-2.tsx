983142ba148924ac9f30abef4bb644cf5833eed3
hs622 <hussainalee@icloud.com>
2026-07-09 17:40:12 +0500

    feat: add ModeToggle2 component for theme switching functionality

---
+++ components/buttons/theme-button-2.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button" 
+import { ButtonGroup } from "../ui/button-group"
+
+export function ModeToggle2() {
+  const { setTheme } = useTheme()
+
+  return (
+    <React.Fragment>
+      <ButtonGroup>
+        <Button variant={"nothing"} className="text-foreground" onClick={() => setTheme("light")}>
+          <Moon />
+        </Button>
+        <Button variant={"nothing"} className="text-foreground" onClick={() => setTheme("dark")}>
+          <Sun />
+        </Button>
+      </ButtonGroup>
+    </React.Fragment>
+  )
+}
