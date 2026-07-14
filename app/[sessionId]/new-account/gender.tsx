cc8a9a09cb1331fc7f55a94a17dd131220e0ba64
hs622 <hussainalee@icloud.com>
2026-07-14 14:53:47 +0500

    feat: enhance AskGenderForm with Controller and FieldDescription for better user experience

--- app/[sessionId]/new-account/gender.tsx
+++ app/[sessionId]/new-account/gender.tsx
@@ -1,43 +1,58 @@
 "use client"
 
-import { Field, FieldError } from "@/components/ui/field" 
+import { Field, FieldDescription, FieldError, FieldLabel } from "@/components/ui/field"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { IMemberFormSchema } from "@/types/zod"
-import { UseFormSetValue, WatchDefaultValue } from "react-hook-form"
+import { Control, Controller, UseFormSetValue } from "react-hook-form"
 import { cn } from "@/lib/utils"
 import { QuestionLabel } from "./_common"
+import { Check } from "lucide-react"
 
 
 export default function AskGenderForm(
-  { error, step, setValue, watch }:
-    { error: string | undefined, step: { id: string, label: string }, setValue: UseFormSetValue<IMemberFormSchema>, watch: WatchDefaultValue<"gender"> }
+  { error, step, setValue, control }:
+    {
+      error: string | undefined,
+      step: { id: string, description?: string, label: string, section: string },
+      setValue: UseFormSetValue<IMemberFormSchema>, control: Control<IMemberFormSchema>
+    }
 ) {
 
   return (
-    <Field data-invalid={!!error} className="gap-4">
-      <QuestionLabel step={step} />
-      <RadioGroup
-        value={watch("gender")}
-        onValueChange={(v) => setValue("gender", v as IMemberFormSchema["gender"], { shouldValidate: true })}
-        className="flex gap-3"
-      >
-        {(["male", "female"] as const).map((g) => (
-          <label
-            key={g}
-            htmlFor={`gender-${g}`}
-            className={cn(
-              "cursor-pointer rounded-full w-35 h-35 flex justify-center items-center border px-5 py-2.5 text-2xl md:text-3xl capitalize transition-colors",
-              watch("gender") === g
-                ? "border-white bg-white text-black dark:border-white"
-                : "border-black text-black hover:bg-black hover:text-white"
-            )}
+    <Controller
+      name="gender"
+      control={control}
+      render={({ field }) => (
+        <Field data-invalid={!!error} className="gap-4">
+          <QuestionLabel step={step} />
+          <RadioGroup
+            name={field.name}
+            value={field.value}
+            onValueChange={(v) => setValue("gender", v as IMemberFormSchema["gender"], { shouldValidate: true })}
+            className="grid grid-cols-2 gap-2"
           >
-            <RadioGroupItem value={g} id={`gender-${g}`} className="sr-only hidden" />
-            {g}
-          </label>
-        ))}
-      </RadioGroup>
-      {error && <FieldError className="text-red-400">{error}</FieldError>}
-    </Field>
+            {(["male", "female"] as const).map((g) => (
+              <FieldLabel
+                key={g}
+                htmlFor={`gender-${g}`}
+                className={cn(
+                  "cursor-pointer flex justify-center items-center px-5 py-2.5 text-9xl bg-transparent",
+                  "has-data-checked:bg-transparent!"
+                )}
+              >
+                <RadioGroupItem
+                  value={g} id={`gender-${g}`} className="sr-only hidden" 
+                />
+                {g === "male" ? "👦🏼" : "👩🏻"}
+
+                <Check className={`${cn("text-black dark:text-white", field.value == g ? "opacity-100" : "opacity-0")}`} />
+              </FieldLabel>
+            ))}
+          </RadioGroup>
+          {step.description && <FieldDescription className="text-black dark:text-white">{step.description}</FieldDescription>}
+          {error && <FieldError className="text-red-400">{error}</FieldError>}
+        </Field>
+      )}
+    />
   )
 }
