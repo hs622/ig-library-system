292d90aa5c98f6abcbd801aa7ce74db73e9200e0
hs622 <hussainalee@icloud.com>
2026-07-14 21:07:25 +0500

    feat: add CreateBookAction_v2 with validation and improved error handling

--- app/actions/createBookAction.ts
+++ app/actions/createBookAction.ts
@@ -3,7 +3,8 @@
 import { ActionResponse } from "@/lib/action-response";
 import clientPromise from "@/lib/mongodb";
 import { normaliseText } from "@/lib/normaliseText";
-import { IBookSchema } from "@/types/zod";
+import { BookCreateSchema } from "@/types/add-book-form.zod";
+import { IBookCreateSchema, IBookSchema } from "@/types/zod";
 import { MongoServerError } from "mongodb";
 
 export const CreateBookAction = async (data: IBookSchema) => {
@@ -46,4 +47,57 @@ export const CreateBookAction = async (data: IBookSchema) => {
       });
   }
 };
+ 
+export const CreateBookAction_v2 = async (data: IBookCreateSchema) => {
+  try {
 
+    data.publicationYear = Number(data.publicationYear) 
+
+    const parsed = BookCreateSchema.safeParse(data);
+    console.log(parsed);
+
+    if (!parsed.success) {
+      return ActionResponse({
+        errors: { ...parsed.error?.errors },
+        message: "validation failed",
+        statusCode: 400,
+      });
+    }
+
+    const { category, tags, ...rest } = parsed.data;
+
+    const client = await clientPromise;
+    const db = client.db(process.env.DATABASE_NAME);
+    const collection = db.collection("books");
+
+    const now = new Date()
+
+    const document = await collection.insertOne({
+      ...rest,
+      tags: tags?.length ? tags : [],
+      createdAt: now,
+      updatedAt: now,
+    });
+
+    return ActionResponse({
+      message: "book created successfully.",
+      statusCode: 201,
+      data: { _id: document.insertedId, ...rest },
+    });
+  } catch (error) {
+    console.log("ACTION creating-book", error);
+    if (error instanceof MongoServerError) {
+      if (error.code === 11000) {
+        return ActionResponse({
+          message: "Book already exists, Please check the book title.",
+          statusCode: 409,
+        });
+      }
+    }
+
+    return ActionResponse({
+      message: "Something went wrong creating the book.",
+      statusCode: 500,
+    });
+  }
+};
