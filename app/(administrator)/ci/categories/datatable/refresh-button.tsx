65474118227f6e42dbecbb7a540c2a114f39e257
hs622 <hussainalee@icloud.com>
2026-07-17 18:54:55 +0500

    feat: Add RefreshButton component for refreshing data in category datatable

---
+++ app/(administrator)/ci/categories/datatable/refresh-button.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { RotateCcw } from "lucide-react";
+
+
+export default function RefreshButton() {
+
+  return (
+    <Button variant={"outline"} >
+      <RotateCcw />
+    </Button>
+  )
+}
