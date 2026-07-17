2f30fc543ee3736ccd7c5db65554ad5d647fbb08
hs622 <hussainalee@icloud.com>
2026-07-17 19:06:07 +0500

    feat: Add members page with UserTable and MemberSearchInput components

---
+++ app/(administrator)/mm/members/page.tsx
@@ -0,0 +1,57 @@
+import { getBaseUrl } from "@/lib/get-base-url";
+import { Metadata } from "next"
+import UserTable from "./datatable/user-table";
+import { MemberSearchInput } from "./datatable/headers";
+
+export const metadata: Metadata = {
+  title: "IGLS — Members"
+}
+
+interface PageProps {
+  searchParams: Promise<{ search?: string }>;
+}
+
+export default async function page({ searchParams }: PageProps) {
+
+  const { search } = await searchParams;
+
+  const baseUrl = getBaseUrl()
+  const params = new URLSearchParams();
+  if (search) params.set("search", search)
+
+  const res = await fetch(`https://igls.localhost/api/users?${params.toString()}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch Members")
+  }
+
+  const { members, nextCursor, hasMore } = await res.json()
+
+  return (
+    <div className="flex flex-col h-[calc(100vh-5rem)] gap-4 p-4">
+      <div className="col-span-2 flex flex-col flex-1 min-h-0">
+        <div className="flex justify-between py-2 shrink-0">
+          <div className="flex items-center gap-4">
+            <MemberSearchInput />
+          </div>
+
+          <div className="flex gap-4">
+            header right
+          </div>
+        </div>
+
+        <div className="flex-1 min-h-0">
+          <UserTable
+            key={search ?? ""}
+            initialData={members}
+            initialCursor={nextCursor}
+            initialHasMore={hasMore}
+            search={search}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
