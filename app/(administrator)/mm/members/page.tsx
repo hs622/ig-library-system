9cad907ccaf256cfa0c1cad028fddf77ee69070b
hs622 <hussainalee@icloud.com>
2026-07-24 23:41:08 +0500

    refactor: Wrap UserTable and dialogs in Suspense for improved loading handling

--- app/(administrator)/mm/members/page.tsx
+++ app/(administrator)/mm/members/page.tsx
@@ -4,6 +4,7 @@ import UserTable from "./datatable/user-table";
 import { MemberSearchInput } from "./datatable/headers";
 import { AddDepositDialog } from "@/components/users/add-users-dialog";
 import { AddUserDrawer } from "@/components/user-drawer";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "IGLS — Members"
@@ -28,12 +29,11 @@ export default async function Page({ searchParams }: PageProps) {
   if (!res.ok) {
     throw new Error("Failed to fetch Members")
   }
-  
+
   const { members, nextCursor, hasMore } = await res.json()
-  // console.log(search, members)
 
   return (
-    <div className="flex flex-col h-[calc(100vh-5rem)] gap-4 p-4">
+    <div className="flex flex-col h-[calc(100vh-4rem)] gap-4 p-4">
       <div className="col-span-2 flex flex-col flex-1 min-h-0">
         <div className="flex justify-between py-2 shrink-0">
           <div className="flex items-center gap-4">
@@ -46,18 +46,23 @@ export default async function Page({ searchParams }: PageProps) {
         </div>
 
         <div className="flex-1 min-h-0">
-          <UserTable
-            key={search ?? ""}
-            initialData={members}
-            initialCursor={nextCursor}
-            initialHasMore={hasMore}
-            search={search}
-          />
+          <Suspense fallback={"loading..."}>
+            <UserTable
+              key={search ?? ""}
+              initialData={members}
+              initialCursor={nextCursor}
+              initialHasMore={hasMore}
+              search={search}
+            />
+          </Suspense>
         </div>
       </div>
-
-      <AddDepositDialog module="user-funds" />
-      <AddUserDrawer module="user-view" />
+      <Suspense fallback={"loading..."}>
+        <AddDepositDialog module="user-funds" />
+      </Suspense>
+      <Suspense fallback={"loading..."}>
+        <AddUserDrawer module="user-view" />
+      </Suspense>
     </div>
   )
 }
