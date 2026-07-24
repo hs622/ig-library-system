7977d9b82f11eeef60ccaf6f10e346d25b879d48
hs622 <hussainalee@icloud.com>
2026-07-24 23:40:28 +0500

    feat: Enhance AddUserDrawer with additional user details and financial records display

--- components/user-drawer.tsx
+++ components/user-drawer.tsx
@@ -1,13 +1,8 @@
 "use client";
 
 import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
+  Drawer, 
+  DrawerContent, 
 } from "@/components/ui/drawer";
 import { Button } from "@/components/ui/button";
 import { useDrawer } from "@/store/useDrawerStore";
@@ -17,7 +12,7 @@ import { Badge } from "./ui/badge";
 import { ScrollArea } from "./ui/scroll-area";
 import { RotateCcw } from "lucide-react";
 import { formatReadableDate } from "@/lib/format-date";
-import { calculateAge } from "@/lib/hepler";
+import { calculateAge, formatCNICNumber, formatContactNumber } from "@/lib/hepler";
 
 export function AddUserDrawer({ module }: { module: string }) {
 
@@ -25,7 +20,7 @@ export function AddUserDrawer({ module }: { module: string }) {
   const resource = useDrawer(s => s.resource)
   const closeDrawer = useDrawer(s => s.closeDrawer)
 
-  const { data, error, isLoading, refetch } = useFetch<IMemberFormSchema & { financialRecords: (IDepositSchema & { _id: string, createdAt: string })[] }>({
+  const { data, error, isLoading, refetch } = useFetch<IMemberFormSchema & { role: string, financialRecords: (IDepositSchema & { _id: string, createdAt: string })[] }>({
     endpoint: `/users/${resource?.resourceId}`,
     searchParams: "w=finance"
   })
@@ -42,42 +37,27 @@ export function AddUserDrawer({ module }: { module: string }) {
       open={isOpen && (resource?.dialog == module)}
       onOpenChange={closeDrawer}
     >
-      <DrawerContent className="max-h-[80vh] h-full">
-        <ScrollArea className="h-full">
+      <DrawerContent className="max-h-[80vh] h-full w-full">
 
-          {/* header */}
-          <div className="flex justify-between items-center px-16 border-b">
-            <div className="pb-4">
-              <div className="text-3xl font-bold">
-                {data?.fullName}
-              </div>
-              <div className="text-md font-medium">
-                {data?.fatherName}
-              </div>
+        {/* header */}
+        <div className="flex justify-between items-center px-16 border-b">
+          <div className="pb-4">
+            <div className="text-3xl font-bold capitalize">
+              {data?.fullName}
             </div>
-
-            <div>
-              <Button variant={"outline"} onClick={refetch} className="cursor-pinter">
-                <RotateCcw />
-              </Button>
+            <div className="text-md font-medium">
+              {data?.fatherName}
             </div>
           </div>
 
-          {/* body */}
-          <div className="px-16 py-6 grid md:grid-cols-2">
-            <div className="flex flex-col gap-4 border-r">
-              <div>
-                <div className="text-lg pb-2">
-                  Area of interest
-                </div>
-                <div className="flex gap-2 flex-wrap">
-                  {data?.areaOfInterest?.map((item, i) => (
-                    <Badge variant={"outline"} key={i}>{item}</Badge>
-                  ))}
-                </div>
-              </div>
+          <div>
+            <Button variant={"outline"} onClick={refetch} className="cursor-pinter">
+              <RotateCcw />
+            </Button>
+          </div>
+        </div>
 
-              <div className="">
+        {/* <div className="">
                 <div>
                   {isJunior
                     ? <Badge variant={"outline"}>Junior</Badge>
@@ -87,50 +67,127 @@ export function AddUserDrawer({ module }: { module: string }) {
                 <div>
 
                 </div>
-              </div>
+              </div> */}
 
-              <div className="p-4">
-                {isLoading ? "loading" : ""}
-                <pre>{JSON.stringify(data, null, 2)}</pre>
-                {error ? error : ""}
-              </div>
-            </div>
+        {/* body */}
+        <div className="px-16 py-6 grid md:grid-cols-2 h-full w-full">
+          <div className="h-full border-r">
+            <ScrollArea className="h-160 px-6">
+              <div className="flex flex-col gap-6">
+                <div>
+                  <div className="text-xl font-bold">Personal Details</div>
 
-            <div className="px-6">
-              <div className="text-xl font-bold pb-4">Financial Records</div>
-              <div className="overflow-hidden ml-4 flex flex-col gap-2">
-                {data?.financialRecords && data?.financialRecords.map((record, i) => (
-                  <div key={i} className="overflow-hidden grid grid-cols-3">
-                    <div className="w-30">
-                      {record.reason}
+                  <div className="pt-2 grid grid-cols-2 gap-2">
+                    <div className="">
+                      <div className="text-sm font-extralight">Email</div>
+                      <div className="capitalize font-semibold" >{data?.email}</div>
+                    </div>
+                  </div>
+                  <div className="pt-2 grid grid-cols-3 gap-2">
+                    <div className="">
+                      <div className="text-sm font-extralight">Role</div>
+                      <div className="capitalize font-semibold" >{data?.role}</div>
                     </div>
                     <div className="">
-                      Rs {record.amount}
+                      <div className="text-sm font-extralight">Gender</div>
+                      <div className="capitalize font-semibold">{data?.gender}</div>
                     </div>
-                    <div>
-                      {formatReadableDate(record.createdAt)}
+                    <div className="">
+                      <div className="text-sm font-extralight">{isJunior ? "B-Form" : "CNIC"}</div>
+                      <div className="capitalize font-extrabold">{formatCNICNumber(String(data?.cnicNumber))}</div>
                     </div>
                   </div>
-                ))}
-              </div>
+                  <div className="pt-2 grid grid-cols-2 gap-2">
+                    <div className="">
+                      <div className="text-sm font-extralight">Address</div>
+                      <div className="capitalize font-semibold" >{data?.address}</div>
+                    </div>
+                    <div className="">
+                      <div className="text-sm font-extralight">{isJunior ? "Guardian Number" : "Contact Number"}</div>
+                      <div className="capitalize font-semibold">+92-{formatContactNumber(String(data?.contactNumber))}</div>
+                    </div>
+                  </div>
+                </div>
 
-              <div>
-                {!data?.financialRecords && (
-                  <div>
-                    Not Financial Records found
+                <div>
+                  <div className="text-lg pb-2">
+                    Area of interest
                   </div>
-                )}
+                  <div className="flex gap-2 flex-wrap">
+                    {data?.areaOfInterest?.map((item, i) => (
+                      <Badge variant={"outline"} key={i}>{item}</Badge>
+                    ))}
+                  </div>
+                </div>
+
+                <div className="">
+                  <div className="text-xl pb-2">
+                    Genres
+                  </div>
+                  <div className="flex gap-1 flex-wrap">
+                    {data?.genre.split(",").map((item, i) => (
+                      <Badge variant={"outline"} key={i}>{item}</Badge>
+                    ))}
+                  </div>
+                </div>
+
+                <div className="">
+                  <div className="text-xl pb-2">
+                    Activities
+                  </div>
+                  <div className="flex gap-1 flex-wrap">
+                    {data?.genre.split(",").map((item, i) => (
+                      <Badge variant={"outline"} key={i}>{item}</Badge>
+                    ))}
+                  </div>
+                </div>
+
+                <div className="">
+                  <div className="text-xl pb-2">
+                    Suggestion
+                  </div>
+                  <div className="w-full text-wrap">
+                    {data?.suggestionForImpovement}
+                  </div>
+                </div>
+
+                {/* <div className="p-4">
+                  {isLoading ? "loading" : ""}
+                  <pre>{JSON.stringify(data, null, 2)}</pre>
+                  {error ? error : ""}
+                </div> */}
               </div>
+            </ScrollArea>
+          </div>
+
+          <div className="px-6">
+            <div className="text-xl font-bold pb-4">Financial Records</div>
+            <div className="overflow-hidden ml-4 flex flex-col gap-2">
+              {data?.financialRecords && data?.financialRecords.map((record, i) => (
+                <div key={i} className="overflow-hidden grid grid-cols-3">
+                  <div className="capitalize w-30">
+                    {record.reason.replaceAll("-", " ")}
+                  </div>
+                  <div className="">
+                    Rs. {record.amount}
+                  </div>
+                  <div>
+                    {formatReadableDate(record.createdAt)}
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <div className="h-140">
+              {!data?.financialRecords.length && (
+                <div className="flex justify-center items-center h-full">
+                  No Financial Records found
+                </div>
+              )}
             </div>
           </div>
-        </ScrollArea>
+        </div>
 
-        {/* <DrawerFooter>
-          <DrawerClose asChild>
-            <Button variant={"outline"}>Close</Button>
-          </DrawerClose>
-        </DrawerFooter> */}
-        {/* </div> */}
       </DrawerContent>
     </Drawer>
   );
