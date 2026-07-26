b5fb9c4c519926d598be9e320b7d8a2904cf226f
hs622 <hussainalee@icloud.com>
2026-07-27 00:03:10 +0500

    feat: Simplify Page component by removing unused props and imports

--- app/g/new-account/page.tsx
+++ app/g/new-account/page.tsx
@@ -1,4 +1,3 @@
-import PageProps from "@/types/props"
 import { Metadata } from "next"
 import NewMemberForm from "./new-member-form"
 
@@ -6,10 +5,7 @@ export const metadata: Metadata = {
   title: "IGLS — New Account"
 }
 
-export default async function Page({ params, searchParams }: PageProps) {
-
-  const resolvedParams = await params
-  const resolvedSearchParams = await searchParams
+export default async function Page() {
 
   return (
     <div className="w-full h-dvh overflow-hidden md:h-screen p-2 sm:p-4">
